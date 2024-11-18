85a53fec8c47801e19f632394d23c2454850483a
july bustamante <july.bustamante@tiendanube.com>
2024-11-18 09:17:21 -0500

    first commit

--- src/Board.js
+++ src/Board.js
@@ -1,15 +1,6 @@
-import { useState } from 'react';
+import Square from "./Square";
 
-function Square({value, onSquareClick}) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-
- function Board({ xIsNext, squares, onPlay }) {
-
+export default function Board(xIsNext, squares, onPlay) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -51,50 +42,7 @@ function Square({value, onSquareClick}) {
       </div>
     </>
   );
-}
-
-export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[currentMove];
-
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
-  }
-
-function jumpTo(nextMove) {
-  setCurrentMove(nextMove);
-  setXIsNext(nextMove % 2 === 0);
-}
-
-const moves = history.map((squares, move) => {
-  let description;
-  if(move > 0){
-    description = 'Go to move #' + move;
-  } else {
-    description = 'Go to the beginning';
-  }
-    return (
-     <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-     </li>
-   );
-  });
-  return (
-    <div className="game">
-      <div className="game-board">
-      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
-      </div>
-      <div className="game-info">
-      <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
+};
 
 function calculateWinner(squares) {
   const lines = [
@@ -105,7 +53,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
